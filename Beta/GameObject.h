e2adcde5f6a845948f60cbb7e5b94d25d00def6e
Nina Ranta <nina.ranta@edu.karelia.fi>
2013-01-18 09:53:33 +0200

    class template - modified class function

---
+++ Beta/GameObject.h
@@ -0,0 +1,68 @@
+////////////////////
+/// C++ Object-oriented Programming
+/// Adventure Game 6
+/// Reference answer 
+/// anssi.grohn@pkamk.fi
+////////////////////
+#ifndef __GameObject_h__
+#define __GameObject_h__
+#include <string>
+#include "Updateable.h"
+#include "Attacker.h"
+////////////////////////////////////////////////////////////////////////////////
+using std::string;
+enum Gender { Male, Female };
+enum Class { Barbarian, Wizard, Paladin, Priest };
+////////////////////////////////////////////////////////////////////////////////
+
+// Base class for all game characters.
+
+// --- Nina Ranta --- added class template 
+template<class TYPE>
+class GameObject : public Attacker, public Updateable
+{
+private:
+  TYPE name;
+  TYPE race;
+  TYPE myClass;
+  TYPE age;
+  TYPE gender;
+  TYPE experience;
+  TYPE hitpoints;
+
+public:
+
+  // --- Nina Ranta --- modified class function to communicate with template
+
+  GameObject();
+  virtual ~GameObject();
+
+  void SetName( TYPE name) ;
+  TYPE GetName() const;
+
+  void SetClass( TYPE c);
+  TYPE GetClass() const;
+
+  void SetAge( TYPE age );
+  TYPE GetAge() const;
+  
+  void SetGender( TYPE g );
+  TYPE GetGender() const;
+  
+  void SetRace( TYPE r );
+  TYPE GetRace() const;
+  
+  void SetExperience( TYPE experience );
+  TYPE GetExperience() const;
+  
+  void Attack( GameObject *pObject );
+
+  TYPE GetHitpoints() const;
+  void SetHitpoints(TYPE hp );
+
+  //---
+
+  void Update();
+};
+
+#endif
