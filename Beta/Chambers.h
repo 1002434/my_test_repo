4025cb51ec29532a9bd7e1a7b5fdcfd349d60e9f
Nina Ranta <nina.ranta@edu.karelia.fi>
2013-01-18 09:52:31 +0200

    Full specialization for boolean type

---
+++ Beta/Chambers.h
@@ -0,0 +1,36 @@
+#ifndef __Chambers_h__
+#define __Chambers_h__
+#include "Room.h"
+#include "Enemy.h"
+#include <sstream>
+////////////////////////////////////////////////////////////////////////////////
+// --- Nina Ranta --- added full specialization for bool type
+template <>
+class Chambers<bool> : public Room
+{
+private:
+  bool waitForIt;
+public:
+  Chambers() : Room(3)
+  {
+    SetDescription("You are in princess' chambers.");
+    Chambers<bool> = true;
+  }
+  void Update()
+  {
+    if ( Chambers<bool> )
+    {
+      Chambers<bool> = false;
+      // ----
+    }
+    else 
+    {
+      // Juha Perala - Added '<<' operator overloading for IRenderer
+      GetGame()->GetRenderer() << "Princess kisses you. You turn into a frog\n";
+      GetGame()->GetRenderer() << "Congratulations, you've completed the game!";
+      GetGame()->SetRunning(false);
+    }
+  }
+};
+////////////////////////////////////////////////////////////////////////////////
+#endif
